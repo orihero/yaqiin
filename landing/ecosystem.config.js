ca7baedb2af457648b7c0b93e8558c196a527cbb
orihero <aka.orihero@gmail.com>
2025-09-14 20:54:52 +0500

    Update ecosystem and configuration files to change application port to 8001 and adjust development script arguments for improved local development experience.

--- landing/ecosystem.config.js
+++ landing/ecosystem.config.js
@@ -10,18 +10,18 @@ module.exports = {
       max_memory_restart: '512M',
       env: {
         NODE_ENV: 'development',
-        PORT: 80
+        PORT: 8001
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 80
+        PORT: 8001
       },
       error_file: './logs/err.log',
       out_file: './logs/out.log',
       log_file: './logs/combined.log',
       time: true,
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      args: 'npm run start'
+      args: 'run dev'
     }
   ]
 };
--- landing/next.config.ts
+++ landing/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  port: 80,
+  host: "0.0.0.0",
 };
 
 export default nextConfig;
