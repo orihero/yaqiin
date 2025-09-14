53c5b7f7f9666d5108a7b4eb8f2ab00a909b09cf
orihero <aka.orihero@gmail.com>
2025-09-14 19:10:42 +0500

    Update ecosystem configuration to change script arguments from 'install' to 'add' for improved package management.

--- landing/ecosystem.config.js
+++ landing/ecosystem.config.js
@@ -21,7 +21,7 @@ module.exports = {
       log_file: './logs/combined.log',
       time: true,
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      args: 'install && yarn build && yarn start'
+      args: 'add && yarn build && yarn start'
     }
   ]
 };
